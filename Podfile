ac8984a1b694e01c2ca542bb6a729f7a99ce4f6f
Marcos Contente <contente.marcos@gmail.com>
2017-02-22 00:54:27 -0300

    config Carthage

--- Podfile
+++
@@ -1,7 +0,0 @@
-platform :ios, '9.0'
-
-target 'SwiftyPop' do
-  use_frameworks!
-  pod 'Moya'
-
-end
--- Pods/Target Support Files/Pods-SwiftyPop/Pods-SwiftyPop-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SwiftyPop : NSObject
-@end
-@implementation PodsDummy_Pods_SwiftyPop
-@end
